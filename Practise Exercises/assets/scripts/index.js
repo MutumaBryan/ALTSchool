6f23a1c9b66ae3120134b84b6e48357e923f456c
MutumaBryan <brayantim22@gmail.com>
2024-08-17 22:20:20 +0300

    some javascript

---
+++ Practise Exercises/assets/scripts/index.js
@@ -0,0 +1,43 @@
+const PRICE = 3.14;
+
+let isOfAge;
+
+isOfAge = 20;
+isOfAge = 25;
+
+console.log(Boolean(""))
+console.log(Boolean(NaN))
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(0))
+console.log(Boolean(false))
+
+let operators = `Arithmetic opertators: - + * / % ++ -- **`;
+
+let i = 2;
+i = i**2;
+i
+let y;
+y = ++i
+y
+
+y = i++
+y
+
+let z = --y
+z
+
+z = y--
+z
+
+let comparisonOperators = `==, ===, !=, >, <, >=, <=`;
+
+let x = typeof NaN
+x
+
+x = Number.isNaN(NaN)
+x
+
+z = z**Math.E
+z
+
